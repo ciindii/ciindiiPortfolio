7d8d26c0862c375ab3ea618d01b957680f4a3a86
Cindy Truong <cindythitruong@gmail.com>
2019-05-26 14:38:56 -0700

    contact added

--- scripts.js
+++ scripts.js
@@ -6,3 +6,15 @@ function openNav() {
 function closeNav() {
   document.getElementById("myNav").style.width = "0%";
 }
+
+//update year for copyright in footer
+crYear = new Date().getFullYear();
+document.getElementById('copyright-year').innerHTML = `${crYear}`;
+
+//shows email address when hovering 'email me'
+function showemail(x) {
+  x.innerHTML = 'cindythitruong';
+}
+function hideemail(x){
+  x.innerHTML =  `email`;
+}
