e09c5aa563ea20689d904776c4ae92d2ddb35894
Cindy Truong <cindythitruong@gmail.com>
2019-05-30 22:54:33 -0700

    added event listener to close menu div

--- scripts.js
+++ scripts.js
@@ -7,6 +7,13 @@ function closeNav() {
   document.getElementById("myNav").style.width = "0%";
 }
 
+//if user click anything other than .menu-links, overlay menu will close
+document.getElementById("myNav").addEventListener("click", function(event){
+  if (event.target.closest(".menu-links")) return;
+  document.getElementById("myNav").style.width = "0%";
+});
+
+
 //update year for copyright in footer
 crYear = new Date().getFullYear();
 document.getElementById('copyright-year').innerHTML = `${crYear}`;
